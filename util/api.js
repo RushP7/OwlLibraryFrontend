5a49cc83a399c45e8be8c2adfff59e697ada0edf
RushP7 <rushilxo5@gmail.com>
2024-05-01 21:34:33 -0400

    api

---
+++ util/api.js
@@ -0,0 +1,13 @@
+const apiUrl = "http://127.0.0.1:8000/api/";
+
+export async function getAllBooks() {
+	const response = await fetch(`${apiUrl}/books/`);
+	const data = await response.json();
+	return data;
+}
+
+export async function getBooksByAuthor(author) {
+	const response = await fetch(`${apiUrl}/books/?author=${author}`);
+	const data = await response.json();
+	return data;
+}
